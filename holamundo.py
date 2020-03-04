34e2de0b941a785df0c350886d65e9d364d575ba
Ximena234 <nikoesamor@gmail.com>
2020-03-04 21:04:13 +0000

    Initial commit

---
+++ holamundo.py
@@ -0,0 +1,8 @@
+# holamundo típico. Muestra un mensaje simple, para ilustrar la estructrura
+# típica de un programa.
+
+# El punto de entrada se llamará main
+# Se compone de un estatuto def
+
+def  main ():
+  print ( "Hola mundo, ahora en Python" )
